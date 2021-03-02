bfbb3730ac1894e3ad3395b558ff6d43cb250060
Ryan Bloom <ryan.bloom@levvel.io>
2021-03-02 13:19:44 -0500

    changed background image for mountain, added blurb about nrdc. Removed blurbs from project information pages

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,8 +6,9 @@ import { Routes } from '../../routes';
 
 export const Home = withRouter((props) =>{
     const urlHash = props.history.location.hash;
-    const aboutMessage:string = 'As a die-hard ski junkie with a passion for art and a year long struggle to find a pair of skis with a graphic that I loved, I decided to take matters into my own hands. I painted my first pair of skis and Ski-On Ski-Art was born. One of a kind, custom, hand-painted skis and boards designed with you and guaranteed to turn heads on the slopes. Reach out to talk about customizing yours today!';
-    
+    const aboutMessage:string = 'As a die-hard ski junkie with a passion for art and a year long struggle to find a pair of skis with a graphic that I loved, I decided to take matters into my own hands. I painted my first pair of skis and Ski-On Ski-Art was born. One-of-a-kind, custom, hand-painted skis and boards guaranteed to turn heads on the slopes.';
+    const missionMessage:string= 'Our climate has quickly become another passion of mine. With climate change becoming more and more dangerous, our ski seasons are at greater risk than ever before. To help protect our environment and our ski seasons, Ski-On Ski-Art is donating 10% of all profits made from commissioned work to the NRDC (National Resource Defense Council).';
+
     const spaceMan = './images/spaceMan.JPG';
     const deepSea = './images/deepSea.JPG';
     const elephant='./images/elephant.JPG';
@@ -26,7 +27,7 @@ export const Home = withRouter((props) =>{
             <div className="header-div">
                 <p className="header-title">Ski-On Ski-Art</p>
                 <h3 className="header-quote">
-                    One of a Kind - Custom - Hand Painted Skis &amp; Snowboards
+                    One-of-a-Kind, Custom, Hand-Painted Skis &amp; Snowboards
                 </h3>
                 <div className="header-nav-menu">
                     <button className="header-nav-button"><Link to="about" smooth={true}>about.</Link></button>
@@ -36,7 +37,9 @@ export const Home = withRouter((props) =>{
             </div>
             <div className="about-div" id="about">
                 <h1 className="section-header">about.</h1>
-                <p className="section-content">{aboutMessage}</p>
+                <p className="section-content">{aboutMessage} <Link className="in-text-link" to="contact" smooth={true}>Reach out</Link> to customize yours today!</p>
+                <hr/>
+                <p className="section-content">{missionMessage} Click <a className="in-text-link" href="https://www.nrdc.org/" target="blank">here</a> to learn more about the NRDC and it's mission!</p>
             </div>
             <div className="paralax-image-2"></div>
             <div className="projects-div" id="projects">
@@ -59,7 +62,7 @@ export const Home = withRouter((props) =>{
             <div className="paralax-image-3"></div>
             <div className="contact-div" id="contact">
                 <h1 className="section-header">contact.</h1>
-                <h2 className="contact-blurb">Reach out via email or instagram to order your skis/board today!</h2>
+                <h2 className="contact-blurb">Reach out via email or instagram to customize your skis/board today!</h2>
                 <div className="contact-info">
                     <a className="email-link" href={`mailto: skionskiart@gmail.com`}>Email: skionskiart@gmail.com</a>
                     <a className="instagram-link" href="https://www.instagram.com/skionskiart" target="_blank">Instagram: @skionskiart</a>
